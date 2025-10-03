6283ad9c4699e123d9855bc950ecf95e54f5fc50
Marshall <msaab.6666@gmail.com>
2025-10-03 19:09:42 +0000

    ضع هذا الرابط https://imgur.com/a/DePSJMU (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image src="https://i.ibb.co/PggVv4j/UTAS-logo.png" alt="EDU Smart Logo" width={120} height={40} />
+      <Image src="https://i.imgur.com/gO05zAP.png" alt="EDU Smart Logo" width={120} height={40} />
     </Link>
   );
 }
