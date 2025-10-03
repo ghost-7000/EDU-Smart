cc624f2ac24d31431e537809fd9897a0a07360da
Marshall <msaab.6666@gmail.com>
2025-10-03 19:20:35 +0000

    ضع هذي صوره على شكل لوجو https://i.ibb.co/h1X4FPxF/5261fa16-9817-4c6a-92

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image src="https://i.imgur.com/gO05zAP.png" alt="EDU Smart Logo" width={120} height={40} />
+      <Image src="https://i.ibb.co/h1X4FPxF/5261fa16-9817-4c6a-924e-9f5aaf3c9d57.jpg" alt="EDU Smart Logo" width={120} height={40} className="object-contain" />
     </Link>
   );
 }
