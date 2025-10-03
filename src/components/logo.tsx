28eab93587d690dab2e73d4715a8667175df7324
Marshall <msaab.6666@gmail.com>
2025-10-03 19:25:58 +0000

    صغر اكثر (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image src="https://i.ibb.co/h1X4FPxF/5261fa16-9817-4c6a-924e-9f5aaf3c9d57.jpg" alt="EDU Smart Logo" width={80} height={28} className="object-contain" />
+      <Image src="https://i.ibb.co/h1X4FPxF/5261fa16-9817-4c6a-924e-9f5aaf3c9d57.jpg" alt="EDU Smart Logo" width={60} height={21} className="object-contain" />
     </Link>
   );
 }
